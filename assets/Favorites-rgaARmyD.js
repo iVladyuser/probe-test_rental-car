97266377b96af44a29515a3d296e90048709e62a
iVladyuser <korol.v09@gmail.com>
2024-03-08 00:22:52 +0100

    Updates

---
+++ assets/Favorites-rgaARmyD.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D2BqEZTS.js";const t=()=>r.jsx("h1",{style:{color:"#3645ab",marginTop:"50px"},children:"Favorites Car"});export{t as Favorites,t as default};
---
+++ assets/Home-BetlgfNQ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D2BqEZTS.js";const t=()=>r.jsx("h1",{style:{color:"#3645ab",marginTop:"50px"},children:"Rental Car"});export{t as Home,t as default};
